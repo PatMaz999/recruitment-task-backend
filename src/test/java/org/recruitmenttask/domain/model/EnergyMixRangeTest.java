3513951f686a75a437f627b684414ae5ed1e7acd
PatMaz999 <Patryk.m.3456@gmail.com>
2025-12-17 18:27:16 +0100

    implement domain model tests

--- src/test/java/org/recruitmenttask/domain/model/EnergyMixRangeTest.java
+++ src/test/java/org/recruitmenttask/domain/model/EnergyMixRangeTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.time.ZonedDateTime;
 import java.util.List;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EnergyMixRangeTest {
@@ -82,7 +83,6 @@ class EnergyMixRangeTest {
         //GIVEN
         ZonedDateTime firstDay = ZonedDateTime.of(2025, 10, 15, 23, 0, 0, 0, EnergyMixRange.TIME_ZONE);
 
-        //first day
         EnergyMixTimestamp t1 = new EnergyMixTimestamp(firstDay, firstDay.plusMinutes(30), List.of(), List.of());
         EnergyMixTimestamp t2 = new EnergyMixTimestamp(firstDay.plusMinutes(30), firstDay.plusMinutes(60), List.of(), List.of());
 
@@ -97,4 +97,88 @@ class EnergyMixRangeTest {
         assertEquals(1, rangeList.size());
     }
 
+
+    @Test
+    void shouldCalculateAverageMixFromMultipleTimestamps() {
+        // GIVEN
+        var wind1 = new EnergyMixTimestamp.EnergySource("Wind", 10.0);
+        var sun1 = new EnergyMixTimestamp.EnergySource("Sun", 20.0);
+        var coal1 = new EnergyMixTimestamp.EnergySource("Coal", 70.0);
+        EnergyMixTimestamp t1 = new EnergyMixTimestamp(
+                MOCK_DATE,
+                MOCK_DATE.plusMinutes(30),
+                List.of(wind1, sun1),
+                List.of(coal1)
+        );
+
+        var wind2 = new EnergyMixTimestamp.EnergySource("Wind", 50.0);
+        var sun2 = new EnergyMixTimestamp.EnergySource("Sun", 40.0);
+        var coal2 = new EnergyMixTimestamp.EnergySource("Coal", 10.0);
+        EnergyMixTimestamp t2 = new EnergyMixTimestamp(
+                MOCK_DATE.plusMinutes(30),
+                MOCK_DATE.plusMinutes(60),
+                List.of(wind2, sun2),
+                List.of(coal2)
+        );
+
+        EnergyMixRange range = new EnergyMixRange(List.of(t1, t2));
+
+        // WHEN
+        EnergyMixTimestamp result = range.mergeTimestamps();
+
+        // THEN
+        assertEquals(2, result.getGreenEnergyPerc().size());
+        assertEquals("Wind", result.getGreenEnergyPerc().getFirst().fuel());
+        assertEquals(30.00, result.getGreenEnergyPerc().getFirst().perc(), 0.01);
+        assertEquals("Sun", result.getGreenEnergyPerc().get(1).fuel());
+        assertEquals(30.00, result.getGreenEnergyPerc().get(1).perc(), 0.01);
+
+        assertEquals(1, result.getOtherEnergyPerc().size());
+        assertEquals("Coal", result.getOtherEnergyPerc().getFirst().fuel());
+        assertEquals(40.0, result.getOtherEnergyPerc().getFirst().perc(), 0.01);
+
+        assertEquals(60.00, result.getTotalGreenPerc());
+    }
+
+    @Test
+    void ShouldReturnTheSameTimestampForSingleElement() {
+        // GIVEN
+        var wind1 = new EnergyMixTimestamp.EnergySource("Wind", 10.0);
+        var sun1 = new EnergyMixTimestamp.EnergySource("Sun", 40.0);
+        var coal1 = new EnergyMixTimestamp.EnergySource("Coal", 90.0);
+        EnergyMixTimestamp t1 = new EnergyMixTimestamp(
+                MOCK_DATE,
+                MOCK_DATE.plusMinutes(30),
+                List.of(wind1, sun1),
+                List.of(coal1)
+        );
+
+        EnergyMixRange range = new EnergyMixRange(List.of(t1));
+
+        // WHEN
+        EnergyMixTimestamp result = range.mergeTimestamps();
+
+        // THEN
+        assertThat(result)
+                .usingRecursiveComparison()
+                .isEqualTo(range.getTimestamps().getFirst());
+    }
+
+    @Test
+    void shouldCreateDetachedCopyOfList() {
+        // GIVEN
+        var t1 = new EnergyMixTimestamp(MOCK_DATE, MOCK_DATE.plusMinutes(30), List.of(), List.of());
+        var t2 = new EnergyMixTimestamp(MOCK_DATE.plusMinutes(30), MOCK_DATE.plusMinutes(60), List.of(), List.of());
+        var t3 = new EnergyMixTimestamp(MOCK_DATE.plusMinutes(60), MOCK_DATE.plusMinutes(90), List.of(), List.of());
+
+        EnergyMixRange originalRange = new EnergyMixRange(List.of(t1, t2, t3));
+
+        // WHEN
+        EnergyMixRange subRange = originalRange.subRange(0, 2);
+        subRange.getTimestamps().clear();
+
+        // THEN
+        assertTrue(subRange.getTimestamps().isEmpty());
+        assertEquals(3, originalRange.getTimestamps().size());
+    }
 }
